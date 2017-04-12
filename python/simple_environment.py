ce10673ab6d658f4872bb752695b2db166102107
Kazu Otani <kazuotani14@gmail.com>
2017-04-12 11:34:15 -0400

    bug fix

--- python/simple_environment.py
+++ python/simple_environment.py
@@ -97,9 +97,9 @@ class SimpleEnvironment(object):
         # self.obstacles.append(cube1)
 
         # T MAP. Assume these are already expanded
-        box1 = np.array([[-50-50], [-50, -40], [50, -40], [50, -50]])
-        box2 = np.array([[-50-25], [-50, 50], [-10, 50], [-10, -25]])
-        box3 = np.array([[20-25], [20, 50], [50, 50], [50, -25]])
+        box1 = np.array([[-50, -50], [-50, -40], [50, -40], [50, -50]])
+        box2 = np.array([[-50, -25], [-50, 50], [-10, 50], [-10, -25]])
+        box3 = np.array([[20, -25], [20, 50], [50, 50], [50, -25]])
         self.obstacles.append(box1)
         self.obstacles.append(box2)
         self.obstacles.append(box3)
