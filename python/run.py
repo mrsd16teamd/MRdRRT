15d65138d29abf54d5d1c63b920a5d4d8a09214f
Aum Jadhav <aum.jadhav@gmail.com>
2017-04-22 21:40:44 -0400

    Working PRM and MRdRRT for simple T-map

--- python/run.py
+++ python/run.py
@@ -13,39 +13,39 @@ from mrdrrt_planner import MRdRRTPlanner
 def main():
     print("Starting...")
     # Map IDs: 1=cube center, 2=T
-    prm = PRMPlanner(n_nodes=300, map_id=2, load=False, visualize=True)
+    prm = PRMPlanner(n_nodes=300, map_id=2, load=True, visualize=True)
 
     test = 3
 
     if test == 1:
         # Test 1 robot in cube map
-        sconfigs = np.array([[0, -30]])
-        gconfigs = np.array([[30, 0]])
+        sconfigs = np.array([[0, -0.3]])
+        gconfigs = np.array([[0.3, 0]])
 
     if test == 2:
         # Test 2 robots in cube map
-        sconfigs = np.array([[0, 30], [-40, -30]])
-        gconfigs = np.array([[40, -30], [0, 20]])
+        sconfigs = np.array([[0, 0.3], [-0.4, -0.3]])
+        gconfigs = np.array([[0.4, -0.3], [0, 0.2]])
 
     if test == 3:
         # Test 2 robots in T map
-        sconfigs = np.array([[-40, -35], [5, -30]])
-        gconfigs = np.array([[5, 30], [15, 35]])
+        sconfigs = np.array([[-0.25, -0.05], [0.25, -0.05]])
+        gconfigs = np.array([[0.25, -0.05], [-0.25, -0.05]])
 
     if test == 4:
         # Test 3 robots in T map
-        sconfigs = np.array([[-40, -35], [5, -30], [0, 0]])
-        gconfigs = np.array([[5, 30], [15, 35], [40, -35]])
+        sconfigs = np.array([[-0.25, -0.05], [0.25, -0.05], [0, 0.25]])
+        gconfigs = np.array([[0.25, -0.05], [0, 0.25], [-0.25, -0.05]])
 
     if test == 5:
         # Test 4 robots in T map
-        sconfigs = np.array([[-40, -35], [5, -30], [0, 0], [20, -35]])
-        gconfigs = np.array([[5, 30], [15, 35], [40, -35], [-20, -38]])
+        sconfigs = np.array([[-0.25, -0.35], [5, -0.3], [0, 0], [0.2, -0.35]])
+        gconfigs = np.array([[0.05, 0.3], [0.15, 0.35], [0.4, -0.35], [-0.2, -38]])
 
     if test == 6:
         # Test 5 robots in T map
-        sconfigs = np.array([[-40, -35], [5, -30], [0, 0], [20, -35], [35, -30]])
-        gconfigs = np.array([[5, 30], [15, 35], [40, -35], [-20, -38], [-5, 45]])
+        sconfigs = np.array([[-0.4, -0.35], [5, -0.3], [0, 0], [0.2, -0.35], [0.35, -0.3]])
+        gconfigs = np.array([[0.05, 0.3], [0.15, 0.35], [0.4, -0.35], [-0.2, -0.38], [-0.05, 0.45]])
 
     mrdrrt = MRdRRTPlanner(prm, n_robots=sconfigs.shape[0], visualize=False)
     path = mrdrrt.FindPath(sconfigs, gconfigs)
